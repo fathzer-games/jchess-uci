4a4e5be937ef0cbc76c167d5f024edd370cea2c3
Fathzer <jm-dev@astesana.net>
2023-06-21 19:59:14 +0200

    Update to comply with games-core changes

--- src/main/java/com/fathzer/jchess/uci/UCI.java
+++ src/main/java/com/fathzer/jchess/uci/UCI.java
@@ -15,11 +15,14 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import com.fathzer.games.MoveGenerator;
 import com.fathzer.games.perft.Divide;
 import com.fathzer.games.perft.PerfT;
 import com.fathzer.games.perft.PerfTResult;
+import com.fathzer.games.util.ContextualizedExecutor;
 import com.fathzer.jchess.uci.option.Option;
 
 /** A class that implements a subset of the <a href="http://wbec-ridderkerk.nl/html/UCIProtocol.html">UCI protocol</a>.
@@ -236,23 +239,25 @@ public class UCI implements Runnable {
 	}
 	
 	protected <M> LongRunningTask<PerfTResult<UCIMove>> getPerfTTask(int depth, int parallelism) {
-		final PerfT<M> perft = new PerfT<>(((UCIMoveGeneratorProvider<M>)getEngine())::getMoveGenerator);
-		perft.setParallelism(parallelism);
-		return new LongRunningTask<PerfTResult<UCIMove>>() {
-			@Override
-			public PerfTResult<UCIMove> get() {
-				final PerfTResult<M> result = perft.divide(depth);
-				final UCIMoveGenerator<M> uciMoveGenerator = (UCIMoveGenerator<M>) perft.buildMoveGenerator();
-				final Function<Divide<M>, Divide<UCIMove>> mapper = d -> new Divide<>(uciMoveGenerator.toUCI(d.getMove()), d.getCount());
-				return new PerfTResult<>(result.getNbMovesMade(), result.getNbMovesFound(), result.getDivides().stream().map(mapper).collect(Collectors.toList()), result.isInterrupted());
-			}
-
-			@Override
-			public void stop() {
-				super.stop();
-				perft.interrupt();
-			}
-		};
+		try (ContextualizedExecutor<MoveGenerator<M>> exec = new ContextualizedExecutor<>(parallelism)) {
+			final PerfT<M> perft = new PerfT<>(exec);
+			return new LongRunningTask<PerfTResult<UCIMove>>() {
+				@Override
+				public PerfTResult<UCIMove> get() {
+					Supplier<MoveGenerator<M>> supplier = (Supplier<MoveGenerator<M>>)getEngine();
+					final PerfTResult<M> result = perft.divide(depth, supplier);
+					final UCIMoveGenerator<M> uciMoveGenerator = (UCIMoveGenerator<M>)supplier.get();
+					final Function<Divide<M>, Divide<UCIMove>> mapper = d -> new Divide<>(uciMoveGenerator.toUCI(d.getMove()), d.getCount());
+					return new PerfTResult<>(result.getNbMovesMade(), result.getNbMovesFound(), result.getDivides().stream().map(mapper).collect(Collectors.toList()), result.isInterrupted());
+				}
+	
+				@Override
+				public void stop() {
+					super.stop();
+					perft.interrupt();
+				}
+			};
+		}
 	}
 
 	private void doPerft(LongRunningTask<PerfTResult<UCIMove>> task, int parallelism) {
